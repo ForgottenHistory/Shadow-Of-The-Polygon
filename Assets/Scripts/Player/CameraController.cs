8d746d2aa63bc806862ef346a3167dd38cb2f15f
Albin Fyhr <albinfyhr@gmail.com>
2023-05-19 21:57:41 +0200

    Basic scene setup
    With playercontroller and camera

--- Assets/Scripts/Player/CameraController.cs
+++ Assets/Scripts/Player/CameraController.cs
@@ -2,17 +2,77 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
+public class CameraController : MonoBehaviour, IInitialize
 {
-    // Start is called before the first frame update
-    void Start()
+    ////////////////////////////////////////////////////////////////////
+    // PUBLIC VARIABLES
+    ////////////////////////////////////////////////////////////////////
+
+    //angle minimums
+    [SerializeField]
+    float xMin = -60f,
+        xMax = 60f;
+
+    //variables
+    public float sens = 2.5f;
+
+    ////////////////////////////////////////////////////////////////////
+    // PRIVATE VARIABLES
+    ////////////////////////////////////////////////////////////////////
+
+    Transform playerTrans = null;
+
+    Vector3 playerOffset = Vector3.zero;
+
+    float rotX = 0.0f;
+    float rotY = 0.0f;
+
+    ////////////////////////////////////////////////////////////////////
+
+    public bool isActive { get; set; } = false;
+
+    ////////////////////////////////////////////////////////////////////
+
+    public void Initialize()
     {
-        
+        playerTrans = transform.parent;
+
+        transform.parent = null;
+
+        // Set cursor to invisible and lock it to the center of the screen
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+        // Activate the script
+        isActive = true;
     }
 
-    // Update is called once per frame
+    ////////////////////////////////////////////////////////////////////
+
+    public void Deinitialize()
+    {
+        // Deactivate the script
+        isActive = false;
+    }
+
+    ////////////////////////////////////////////////////////////////////
+
     void Update()
     {
-        
+        if (isActive == false) return;
+
+        transform.position = playerTrans.position + playerOffset;
+
+        //rotations
+        rotX += Input.GetAxis("Mouse Y") * sens;
+        rotY += Input.GetAxis("Mouse X") * sens;
+
+        rotX = Mathf.Clamp(rotX, xMin, xMax); //limit player
+
+        //change rotations
+        transform.eulerAngles = new Vector3(-rotX, rotY, transform.eulerAngles.z);
+        playerTrans.rotation = Quaternion.Euler(playerTrans.rotation.x, transform.eulerAngles.y, playerTrans.rotation.z);
     }
+
+    ////////////////////////////////////////////////////////////////////
 }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,18 +1,113 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : MonoBehaviour, IInitialize
 {
-    // Start is called before the first frame update
-    void Start()
+    ////////////////////////////////////////////////////////////////////
+    // PUBLIC VARIABLES
+    ////////////////////////////////////////////////////////////////////
+
+    public float speed = 3.0f;
+    public float rotateSpeed = 3.0f;
+
+    public float jumpSpeed = 8.0f;
+
+    public float gravity = 1000.0f;
+
+    ////////////////////////////////////////////////////////////////////
+    // PRIVATE VARIABLES
+    ////////////////////////////////////////////////////////////////////
+
+    CharacterController controller;
+    CameraController cameraController;
+    Vector3 moveDirection = Vector3.zero;
+
+    private float verticalVelocity = 0.0f;
+
+    ////////////////////////////////////////////////////////////////////
+
+    public bool isActive { get; set; } = false;
+
+    ////////////////////////////////////////////////////////////////////
+
+    public void Initialize()
     {
-        
+        controller = GetComponent<CharacterController>();
+        cameraController = GetComponentInChildren<CameraController>();
+        cameraController.Initialize();
+
+        isActive = true;
     }
 
-    // Update is called once per frame
+    ////////////////////////////////////////////////////////////////////
+
+    public void Deinitialize()
+    {
+        isActive = false;
+    }
+
+    ////////////////////////////////////////////////////////////////////
+
     void Update()
     {
-        
+        if (!isActive) return;
+
+        Movement();
     }
+
+    ////////////////////////////////////////////////////////////////////
+
+    void Movement()
+    {
+        // Get the inputs for horizontal and vertical axis (WASD or arrow keys by default)
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        // Create a new vector of direction for the character
+        Vector3 move = new Vector3(moveHorizontal, 0, moveVertical);
+        move = transform.TransformDirection(move);
+
+        // Multiply it by the speed
+        move *= speed;
+
+        Jump();
+
+        // Apply gravity to the vertical velocity
+        verticalVelocity -= gravity * Time.deltaTime;
+
+        // Assign verticalVelocity to moveDirection.y
+        move.y = verticalVelocity;
+
+        // Move the character
+        controller.Move(move * Time.deltaTime);
+    }
+    
+    ////////////////////////////////////////////////////////////////////
+
+    void Jump()
+    {
+        // If the character is grounded and the player presses "Jump", then the character jumps
+        if (controller.isGrounded)
+        {
+            // If we're grounded, the vertical velocity should be 0
+            verticalVelocity = 0;
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                verticalVelocity = jumpSpeed;
+            }
+        }
+    }
+
+    ////////////////////////////////////////////////////////////////////
 }
+
+////////////////////////////////////////////////////////////////////
+
+public interface IInitialize
+{
+    public bool isActive { get; set; }
+    public void Initialize();
+    public void Deinitialize();
+}
+
+////////////////////////////////////////////////////////////////////
